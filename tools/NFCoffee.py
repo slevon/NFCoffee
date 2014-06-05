0035dd7591d4162f9f1dda4f8f84cd0a3f79bfdc
slevon <r.raekow@gmail.com>
2014-06-06 00:03:54 +0200

    Creating a almost working GUI and did some work in NFCoffee Class

---
+++ tools/NFCoffee.py
@@ -0,0 +1,149 @@
+__author__ = 'Roman'
+
+
+import xlsxwriter
+from xlsxwriter.utility import xl_rowcol_to_cell
+from xlsxwriter.utility import xl_range
+import sys
+import os
+import time
+import zipfile
+
+
+
+class NFCoffee():
+    def __init__(self):
+        # init attibutes:
+        #The Dict with the User ID, Name and count of Coffees, where the ID is the Key
+        self.data = {}
+        self.mMinimumCoffees = 8    # 3EUR at the beginning
+        self.mPrice = 0.4             # 0,4 EU
+        self.errors=[]
+
+    def readData(self, path=""):
+
+        #read the user file
+        try:
+            with open(path + 'USER.TXT') as file:  # Use file to refer to the file object
+                for line in file:
+                    uuid, name = line.replace("\n", '').split("\t", maxsplit=1)
+                    #Check is we hav two keys in the file -> Something went terribly wrong
+                    if uuid in self.data:
+                        raise Exception("Error 1: duplicate ID in USER.TXT")
+                        return
+                        #if not: we add it to our dict
+                    self.data[uuid] = {'uuid': uuid, 'name': name, 'count': 0}
+        except Exception as e:
+            raise Exception("File USER.TXT not found, or it contains errors")
+
+        #read the coffe-file.
+        try:
+            with open(path + 'COUNT.TXT') as file:  # Use file to refer to the file object
+                for line in file:
+                    uuid, count = line.replace("\n", '').split("\t", maxsplit=1)
+                    try:
+                        self.data[uuid]['count'] = int(count)
+                    except Exception as e:
+                        print(e)
+                        print("Unknown UUID", uuid, "found in COUNTS.txt")
+                        self.errors.append("Unknown UUID "+ uuid+ " found in COUNTS.txt")
+                        self.data[uuid]={'uuid': uuid, 'name': '???'}
+                        self.data[uuid]['count'] = int(count)
+        except Exception as e:
+            raise Exception("File COUNT.TXT not found, or it contains errors")
+
+    '''
+        OLD ort the DICT by Name by creating a List with UUIDs in the correct order
+        #sortedCounts=sorted(counts.items(), key=lambda x: x[1]['name'])
+        #now we have a list like so
+        #[('AABBEE9F',{'uuid':'AABBEE9F','name':'Adam','count':3},
+        #  ...]
+
+        # Clear the old container to free memory
+        counts=None
+    '''
+
+    def setPrice(self, price):
+        self.mPrice = price
+        print('NFCoffee.setPrice: ' +str(self.mPrice))
+
+    def price(self):
+        return self.mPrice
+
+    def setMinimumCoffees(self,minimumCoffeeAmount):
+        self.mMinimumCoffees = minimumCoffeeAmount
+        print('NFCoffee.setMinimumCoffees: ' +str(self.mMinimumCoffees))
+
+    def minimumCoffees(self):
+        return self.mMinimumCoffees
+
+    def export(self, filename=None):
+
+        # ################################
+        # Create the excel sheet
+        #################################
+        # Create a workbook and add a worksheet.
+        if filename == None:
+            filename = 'Abrechnung_'+time.strftime("%Y-%m-%d_%H-%M-%S")
+
+        workbook = xlsxwriter.Workbook(filename + '.xlsx')
+        worksheet = workbook.add_worksheet('Kaffee-Abrechnung')
+
+        # Add a number format for cells with money.
+        moneyFormat = workbook.add_format({'num_format': u'0.00€'})
+        resultFormat = workbook.add_format({'bold': True, 'top': 1})
+
+        # Start from the first cell. Rows and columns are zero indexed.
+        row = 1
+        col = 0
+        worksheet.write(0, 0, 'Name')
+        worksheet.set_column(0, 0, 50)
+        worksheet.write(0, 1, 'Anzahl')
+        worksheet.write(0, 2, 'Betrag')
+        worksheet.set_column(2, 2, 10, moneyFormat)
+        worksheet.write(0, 3, '€/Kaffe:')
+        worksheet.write(0, 4, pPrice, moneyFormat)
+        # Iterate over the data and write it out row by row.
+        # OLD Version without sorting for key, item in counts.items():
+        for item in self.mCounts:
+            worksheet.write(row, col, item[1]['name'])
+            worksheet.write(row, col + 1, item[1]['count'])
+            worksheet.write(row, col + 2, '=PRODUCT(E1,' + xl_rowcol_to_cell(row, col + 1) + ')')
+            row += 1
+
+        # Write a total using a formula.
+        worksheet.write(row, 0, 'Total', resultFormat)
+        worksheet.write(row, 1, '=SUM(' + xl_range(1, 1, row - 1, 1) + ')', resultFormat)
+        worksheet.write(row, 2, '=SUM(' + xl_range(1, 2, row - 1, 2) + ')', resultFormat)
+
+        workbook.close()
+
+        print('File: ' + filename + '.xlsx created')
+
+        #########################################
+        ## Remove the old Count File and back it up
+        #########################################
+        zip = zipfile.ZipFile("count.zip", "a")
+        zip.write("COUNT.TXT", filename + ".TXT")
+        zip.close()
+
+        # Everthing is fine now, so delete the old Backup and write back the Counts of the users with
+        # few coffes this month
+        try:
+            os.remove('~COUNT.TXT')
+        except (OSError, IOError) as e:
+            pass
+
+        os.rename('COUNT.TXT', '~COUNT.TXT')
+
+    @property
+    def getData(self):
+        return list(self.data.values())
+
+    def getErrors(self):
+        erros=self.errors
+        self.errors=[]
+        return erros
+
+
+
--- tools/main.py
+++ tools/main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+'''
+This is the GUI Managment Tool for NFCoffee
+
+
+
+
+    icons taken from http://dryicons.com
+
+'''
+
+from NFCoffee import NFCoffee
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+
+import configparser
 
 import sys
 import json
@@ -27,7 +40,7 @@ class AddUserDialog(QDialog):
             self.mLayout.addWidget(QLabel('Name:'), 2, 0)
             self.mNameEdit=QLineEdit()
             self.mLayout.addWidget(self.mNameEdit, 2, 1)
-            buttonBox =  QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+            buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
             buttonBox.accepted.connect(self.checkData)
             buttonBox.rejected.connect(self.reject)
             #connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
@@ -40,46 +53,58 @@ class AddUserDialog(QDialog):
         if self.mUuidLineEdit.text().__len__() == 10 and self.mNameEdit.text().__len__() > 3:
             self.accept()
         else:
-             QMessageBox.critical(None, "Fehler",
-                "<h2>Fehler</h2>Die ID muss exact <b>fünf</b> Byte lang sein.<br/>Der Name mehr als <b>drei</b> Zeichen haben.")
+            QMessageBox.critical(None, "Fehler",
+                "<h2>Fehler</h2>"
+                "Die ID muss exact <b>fünf</b> Byte lang sein.<br/>Der Name mehr als <b>drei</b> Zeichen haben.")
 
 
     def getUUID(self):
-        return self.mUuidLineEdit.text();
+        return self.mUuidLineEdit.text()
+
     def getName(self):
-        return self.mNameEdit.text();
-    def setUUID(self,text):
-        self.mUuidLineEdit.setText(text);
-    def setName(self,text):
-        self.mNameEdit.setText(text);
+        return self.mNameEdit.text()
 
+    def setUUID(self, text):
+        self.mUuidLineEdit.setText(text)
 
+    def setName(self, text):
+        self.mNameEdit.setText(text)
 
+##########################################################################################
+##########################################################################################
+##########################################################################################
 
 class MainWidget(QWidget):
 
     def __init__(self, parent=None):
         super(MainWidget, self).__init__(parent)
-        self.mTable = QTableWidget(1,4,self)
+
+        self.mTable = QTableWidget(0,4,self)
+        header = [ "UUID", "Name", "Anzahl", "Betrag"]
+        self.mTable.setHorizontalHeaderLabels(header)
+        self.mTable.horizontalHeader().setResizeMode(1, QHeaderView.Stretch)
+        self.mTable.setContextMenuPolicy(Qt.CustomContextMenu)
         self.mTable.setSortingEnabled(True)
-        self.initTable()
+        self.mTable.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.mTable.setColumnWidth(1,250)
 
         self.mTextEdit=QTextEdit(self)
         self.mTextEdit.setFont(QFont ("Trebuchet,sans-serif", 9))
         self.mTextEdit.setTabChangesFocus(False)
 
-        self.logText("Starting...");
+        self.logText("Starting...")
 
         self.mSplitter=QSplitter(Qt.Vertical,self)
         self.mSplitter.addWidget(self.mTable)
         self.mSplitter.addWidget(self.mTextEdit)
-        self.mSplitter.setStretchFactor (0, 100)
+        self.mSplitter.setStretchFactor(0, 100)
         self.mSplitter.setStretchFactor(1, 1)
 
         self.mLayout=QGridLayout()
-        self.mLayout.addWidget(self.mSplitter,0,0)
+        self.mLayout.addWidget(self.mSplitter, 0, 0)
 
         self.setLayout(self.mLayout)
+
     def logNewSection(self):
             self.mTextEdit.append("<hr/>")
 
@@ -90,21 +115,65 @@ class MainWidget(QWidget):
 
             self.mTextEdit.append(string.replace("\n","<br/>")+"<br/>")
 
-    def initTable(self):
-        header = [ "UUID", "Name", "Anzahl", "Betrag"]
-        self.mTable.setHorizontalHeaderLabels(header)
-        self.mTable.horizontalHeader().setResizeMode(1, QHeaderView.Stretch)
-        self.mTable.setContextMenuPolicy(Qt.CustomContextMenu)
+    def initTable(self, data):
+        """
 
-        for x in range(0, 3):
-            row=self.mTable.rowCount()
+        :param data: A List of dics with uuid,name and count as keys
+        """
+
+        self.mTable.clear()
+        self.mTable.setRowCount(0)
+
+        for item in data:
+            row = self.mTable.rowCount()
             self.mTable.insertRow(row)
+            #Name
+            self.mTable.setItem(row, 1, self.__createItem(str(item['name'])))
+            #count
+            self.mTable.setItem(row, 2, self.__createItem(str(item['count'])))
+            #Price
+            price = self.__createItem('')
+            price.setTextAlignment(Qt.AlignRight)
+            self.mTable.setItem(row, 3, price)
+            #UUID
+            #For some verrrrry strange reason this line has to be the last in this loop
+            self.mTable.setItem(row, 0, self.__createItem(str(item['uuid'])))
 
-            uuid=QTableWidgetItem(str(x)*3)
-            uuid.setFlags(uuid.flags() & ~Qt.ItemIsEditable & ~Qt.ItemIsDragEnabled & ~Qt.ItemIsDropEnabled)
 
-            self.mTable.setItem(row, 0, uuid )
-            self.mTable.setItem(row, 1, QTableWidgetItem(str(x)) )
+
+    def __createItem(self, text):
+        twItem = QTableWidgetItem(text)
+        twItem.setFlags(twItem.flags() & ~Qt.ItemIsEditable & ~Qt.ItemIsDragEnabled & ~Qt.ItemIsDropEnabled)
+        return twItem
+
+    def recalcAmount(self, price):
+        """
+
+        :param price: The price in EUR for one coffee
+        """
+        rows = self.mTable.rowCount()
+        for row in range(0, rows):
+            try:
+                count = int(self.mTable.item(row,2).text())
+                self.mTable.item(row, 3).setText("{0:.2f} €".format((count*price)))
+            except Exception as e:
+                print(e)
+
+    def markMinimumCoffees(self,minimum):
+        rows = self.mTable.rowCount()
+        cols = self.mTable.columnCount()
+        for row in range(0, rows):
+            #try:
+                count = int(self.mTable.item(row,2).text())
+                if count < minimum:
+                    for col in range(0,cols):
+                        self.mTable.item(row,col).setBackground(QColor(250,220,220))
+                else:
+                    for col in range(0,cols):
+                        self.mTable.item(row,col).setBackground(QColor(255,255,255))
+            #except Exception as e:
+            #    pass
+
 ###########################################################################################################
 ###########################################################################################################
 ###########################################################################################################
@@ -113,13 +182,20 @@ class MyMainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         #init attributes:
-        self.mMainWidget=MainWidget(self)
+
+        #We have our working Class, that will do all the work on the files:
+        #TODO NFC Class usage
+        self.mNFCoffee = NFCoffee()
+        self.mNFCoffee.readData()
+
+        self.mMainWidget = MainWidget(self)
+
         self.setCentralWidget(self.mMainWidget)
         self.resize(600,400)
         self.statusBar().showMessage('Ready')
 
-        self.mPopMenu=QMenu()
-        self.mMainMenu=self.menuBar()
+        self.mPopMenu = QMenu()
+        self.mMainMenu = self.menuBar()
 
         self.connect(self.mMainWidget.mTable,SIGNAL('customContextMenuRequested(QPoint)'), self.onContextMenu)
         #self.connect(self.mMainMenu,SIGNAL('triggered(QAction)'), self.onMainMenu)
@@ -147,13 +223,50 @@ class MyMainWindow(QMainWindow):
 
         self.aExport = QAction(QIcon('icons/download_page.png'), 'Report erstellen', self)
         self.aExport.setShortcut('Ctrl+E')
-        self.aExport.triggered.connect(self.addUser)
+        self.aExport.triggered.connect(self.createReport)
         self.toolbarOptions.addAction(self.aExport)
 
+        # PriceBox
+        self.toolbarOptions.addSeparator()
+        self.toolbarOptions.addWidget(QLabel("Preis/Kaffee:"))
+        self.aPriceBox = QDoubleSpinBox()
+        self.aPriceBox.setSingleStep(0.05)
+        self.aPriceBox.setSuffix("€")
+        # TODO Save and restore settings
+        self.aPriceBox.setValue(self.mNFCoffee.price())
+        self.aPriceBox.valueChanged.connect(self.updatePrice)
+        self.toolbarOptions.addWidget(self.aPriceBox)
+        self.toolbarOptions.addSeparator()
+        # Minimum Coffees
+        self.toolbarOptions.addWidget(QLabel("Mindesanzahl an Kaffees:"))
+        self.aMinimumBox = QSpinBox()
+        self.aMinimumBox.setToolTip("Geben sie hier die Mindestanzahl der Kaffees an, die erreicht werden muss damit die"
+                                    " Kaffees abgerechnet werden.\nAlle anderen bleiben erhalten.")
+        # TODO Save and restore settings
+        self.aMinimumBox.setValue(self.mNFCoffee.minimumCoffees())
+        self.aMinimumBox.valueChanged.connect(self.updateMinimumCoffees)
+        self.toolbarOptions.addWidget(self.aMinimumBox)
+        self.toolbarOptions.addSeparator()
+
+        #Save the settings
+        self.restoreSettings()
+
+        #init the Table
+        self.mMainWidget.initTable(self.mNFCoffee.getData)
+        self.mMainWidget.recalcAmount(self.mNFCoffee.price())
+        self.mMainWidget.markMinimumCoffees(self.mNFCoffee.minimumCoffees())
+
+        #Show all errors
+        self.mMainWidget.logText("\n".join(self.mNFCoffee.getErrors()))
 
 
+    def closeEvent(self, evnt):
+        self.saveSettings()
+        super(MyMainWindow, self).closeEvent(evnt)
 
     def onContextMenu(self,point):
+
+
         treeItem=self.mMainWidget.mTable.itemAt(point)
         #find the toplevel item
         while treeItem.parent():
@@ -178,11 +291,46 @@ class MyMainWindow(QMainWindow):
 
     def addUser(self):
         dia = AddUserDialog()
-        dia.exec()
+        dia.exec_()
     def deleteUser(self):
         # TODO Delete user function
-        QMessageBox.critical(None, "Fehler",
+        QMessageBox.critical(None, "Fehler Delete",
                 "<h2>:-( TODO</h2>")
+
+
+    def createReport(self):
+        # TODO REport/Export Function
+        QMessageBox.critical(None, "Fehler Report",
+                "<h2>:-( TODO</h2>")
+
+    def updatePrice(self, price):
+        self.mNFCoffee.setPrice(price)
+        self.mMainWidget.recalcAmount(price)
+
+
+    def updateMinimumCoffees(self,minimumCoffees):
+        self.mNFCoffee.setMinimumCoffees(minimumCoffees)
+        self.mMainWidget.markMinimumCoffees(minimumCoffees)
+
+    def saveSettings(self):
+        config = configparser.ConfigParser()
+        config.read('NFCoffee.INI')
+        #TODO all Config
+        config['COFFEE']['price'] = str(self.mNFCoffee.price())
+        config['COFFEE']['minimumCoffees'] = str(self.mNFCoffee.minimumCoffees())
+
+        with open('NFCoffee.INI', 'w') as configfile:    # save
+            config.write(configfile)
+
+        self.mMainWidget.logText("Ini File updated...")
+
+    def restoreSettings(self):
+        config = configparser.ConfigParser()
+        config.read('NFCoffee.INI')
+        self.mMainWidget.logText("Reading Ini File...")
+        #TODO all Config
+        self.aPriceBox.setValue(float(config['COFFEE']['price']))
+        self.aMinimumBox.setValue(int(config['COFFEE']['minimumCoffees']))
 
 app = QApplication([])
 mW = MyMainWindow()
